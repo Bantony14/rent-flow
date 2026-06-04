d5735e42c3d2b1bdc994a051427cea86056c971f
bantony <bantonysin95@gmai.com>
2026-06-04 10:22:01 -0700

    little update

--- client/src/assets/pages/AdminPages/AdminDashboard.jsx
+++ client/src/assets/pages/AdminPages/AdminDashboard.jsx
@@ -9,41 +9,7 @@ const AdminDashboard = () => {
     const [search, setSearch] = useState("");
     const placeholder = "search by email,mobileNumber or Name"
 
-    const user = {
-        _id: "6a117401a76a103b1b5024aa",
-        fullName: "Rahul Sharma",
-        email: "rahul@example.com",
-        mobileNumber: "9876543210",
-        dob: "15 May 2002",
-        roomNumber: "101",
-        building: "Shivam Residency",
-        role: "USER",
-        rentPrice: 5000,
-        paymentStatus: true,
-        createdAt: "23 May 2026",
-        updatedAt: "23 May 2026",
-    };
 
-    const users = [
-        {
-            _id: "1",
-            name: "Rahul Sharma",
-            email: "rahul@gmail.com",
-            role: "Tenant",
-        },
-        {
-            _id: "2",
-            name: "Aman Verma",
-            email: "aman@gmail.com",
-            role: "Owner",
-        },
-        {
-            _id: "3",
-            name: "Raj Patel",
-            email: "raj@gmail.com",
-            role: "Admin",
-        },
-    ];
 
     return (
         <div className="min-h-screen bg-slate-50 p-6">
--- server/config/db.connect.js
+++ server/config/db.connect.js
@@ -1,15 +1,19 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
 config();
-
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const databaseConnection = async () => {
 
     try {
+
+        console.log(process.env.MONGO_DB_URL)
         const db = await mongoose.connect(process.env.MONGO_DB_URL)
+
         console.log("✅ mongoDb connected sucessfully", db.connection.host)
 
     } catch (error) {
-        console.log(error.message);
+        console.log(error);
         process.exit(1)
     }
 
