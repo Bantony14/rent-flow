1cd3873fb180dcd5ee17dd5c1217aed356dc11bc
bantony <bantonysin95@gmail.com>
2026-07-22 17:52:35 +0530

    change BaseUrl For connect server

--- client/src/assets/api/paymentApi.js
+++ client/src/assets/api/paymentApi.js
@@ -1,18 +1,18 @@
-import axios from "axios"
+import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/v1/payment",
-    withCredentials: true
-})
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1/payment`,
+  withCredentials: true,
+});
 
 export function paymentOrderCreate() {
-    return API.post("/create-order")
+  return API.post("/create-order");
 }
 
 export function paymentCheck() {
-    return API.post("/payment-check")
+  return API.post("/payment-check");
 }
 
 export function paymentHistoryByUser() {
-    return API.get("/payment-history")
+  return API.get("/payment-history");
 }
