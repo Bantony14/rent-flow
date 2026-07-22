d1155b75429bfeccad87807f215fd5706788fcf3
bantony <bantonysin95@gmail.com>
2026-07-22 23:28:36 +0530

    fix in client

--- client/src/assets/api/paymentApi.js
+++ client/src/assets/api/paymentApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: `${import.meta.env.VITE_API_URL}/api/v1/payment`,
-  baseURL: "http://localhost:5000/api/v1/payment",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1/payment`,
+  //  baseURL: "http://localhost:5000/api/v1/payment",
   withCredentials: true,
 });
 
