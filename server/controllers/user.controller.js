7d00bce0dc27b3973555cb0ce19201ded5a0aace
bantony <bantonysin95@gmail.com>
2026-07-22 12:03:03 +0530

    final deployed code

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -452,6 +452,8 @@ export const userLogin = async (req, res, next) => {
     const token = user.jwtToken();
     res.cookie("token", token, {
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     user.password = undefined;
